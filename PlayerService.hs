cafa7bcf891cd56fe3718ab949edf8976cb3686c
Randall Britten <randall.britten@gmail.com>
2016-05-08 22:00:11 +1200

    Some Hlint tips applied

--- PlayerService.hs
+++ PlayerService.hs
@@ -1,4 +1,3 @@
-import Control.Applicative ((<$>))
 import Data.Aeson (eitherDecode', Object)
 import Data.ByteString.Lazy (append, fromStrict)
 import Data.ByteString.Lazy.Char8 (pack)
@@ -7,6 +6,8 @@ import Network.Wai.Parse (parseRequestBody, lbsBackEnd)
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (hServer, hContentType, status200, status400, methodPost)
 import System.Environment (lookupEnv)
+import Data.Maybe (fromMaybe)
+import Control.Monad ((>=>))
 
 import Player
 
@@ -21,20 +22,20 @@ handler :: Application
 handler request respond = if methodPost == requestMethod request
   then do
     (params, _) <- parseRequestBody lbsBackEnd request
-    let getParam n v = maybe v id $ lookup n params
+    let getParam n v = fromMaybe v $ lookup n params
         action       = getParam "action" "version"
         state        = parseJSON $ getParam "game_state" "{}" :: Either String Object
         withState f  = either badRequest f state
     case action of
       "check"       -> sayVersion
       "version"     -> sayVersion
-      "bet_request" -> withState $ \s -> betRequest s >>= ok . pack . show
+      "bet_request" -> withState $ betRequest >=> ok . pack . show
       "showdown"    -> withState $ \s -> showdown s >> ok ""
       _             -> badRequest "unknown action"
   else sayVersion
   where
     parseJSON = eitherDecode' . fromStrict
-    sayVersion = ok $ pack $ version
+    sayVersion = ok $ pack version
     ok = send status200
     badRequest = send status400 . append "Bad request: " . pack
     send status = respond . responseLBS status headers
